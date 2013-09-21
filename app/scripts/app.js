7d2adbf75ef4f078d0dad862c9663616a9656ebc
Teemu Lahti <teemu.lahti@gmail.com>
2013-09-22 01:55:49 +0300

    social share icons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ app.nextVideo = function () {
     }
 }
 
-app.onVideoPlaying = function() {
+app.onVideoPlaying = function () {
     var videoUrl = app.player.getVideoUrl()
         , paramArr = videoUrl.split('?')[1].split('&')
         , params = []
@@ -31,7 +31,7 @@ app.onVideoPlaying = function() {
     $('#ytcontainer').show();
 }
 
-app.toggleInfo = function() {
+app.toggleInfo = function () {
     $('#authorinfo div.content').toggleClass('visible');
 }
 
@@ -40,14 +40,40 @@ $(document).ready(function () {
         e.preventDefault();
         app.nextVideo();
     });
-    $('#authorinfo a.toggle').click(function(e) {
+    $('a.share.fb').click(function (e) {
+        e.preventDefault();
+        FB.ui({
+            method:      'feed',
+            link:        'http://aciddose.org/discorobot/',
+            name:        '&hearts; DISCO &hearts;',
+            picture:     'http://aciddose.org/discorobot/images/italo.png',
+            caption:     'Discorobot',
+            description: 'Discorobot: Non-stop italo/synth disco goodness'
+        });
+    });
+    $('a.share.twitter').click(function (e) {
+        e.preventDefault();
+        e.preventDefault();
+        var url = 'http://twitter.com/share?' +
+            'url=' + escape('http://aciddose.org/discorobot/') +
+            '&text=Discorobot: Non-stop disco';
+        window.open(url, 'twitter', 'width=700,height=360,location=0,status=0');
+    });
+    $('a.share.gplus').click(function (e) {
+        e.preventDefault();
+        var url = 'https://plus.google.com/share?' +
+            'url=' + escape('http://aciddose.org/discorobot/');
+        window.open(url, 'gplus', 'width=500,height=460,location=0,status=0');
+    });
+    $('#authorinfo a.toggle').click(function (e) {
+
         e.preventDefault();
         app.toggleInfo();
-    })
-    $(app).on('player:finished', function(e) {
+    });
+    $(app).on('player:finished', function (e) {
         app.nextVideo();
-    })
-    $(app).on('player:playing', function(e) {
+    });
+    $(app).on('player:playing', function (e) {
         app.onVideoPlaying();
-    })
+    });
 });
